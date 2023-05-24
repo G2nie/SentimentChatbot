e3c82a97f2094e0f4d967c8a17ea55814ad1e410
G2nie <202144081@itc.ac.kr>
2023-05-25 00:03:38 +0900

    ver 0.1.1 분석 결과 추가

--- emotion/views.py
+++ emotion/views.py
@@ -5,29 +5,24 @@ from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import json
 
-# 사용자가 입력한 메시지를 받아온 후,
-# 메시지를 분석하여 가장 유사한 대답을 찾아서 리턴
-
 def sentiment_chatbot(request):
     if request.method == 'POST':
         user_input = request.POST.get('user_input')
 
-        # 모델과 데이터 로드
         model = SentenceTransformer('jhgan/ko-sroberta-multitask')
-        df = pd.read_csv('D:\python\Sentimentchatbot\wellness_dataset.csv')
+        df = pd.read_csv('D:\python\SentimentChatbot\wellness_dataset.csv')
         df['embedding'] = df['embedding'].apply(json.loads)
 
-        # sentenceTranformer 모델을 이용하여 사용자가 입력한 문장의 임베딩 계산
-        # 임베딩 = 텍스트 데이터를 벡터 형태로 변환
         embedding = model.encode(user_input)
-
-        # cosine similarity
-        # 유사도 계산 후 가장 유사한 문장 선택
         df['distance'] = df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
         answer = df.loc[df['distance'].idxmax()]
 
-        # 선택된 문장을 반환
-        return JsonResponse({'result': answer['챗봇']},
+        # 구분 컬럼이 존재하는지 확인 후 반환
+        sentiment = answer.get('구분', 'Unknown')
+
+        result = answer['챗봇'] if '챗봇' in answer else 'Unknown'
+
+        return JsonResponse({'result': result, 'sentiment': sentiment},
                             json_dumps_params={'ensure_ascii': False})
 
     return render(request, 'emotion.html')
